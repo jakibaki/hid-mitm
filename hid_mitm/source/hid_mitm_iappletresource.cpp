ad1a275f810a5dd3b2d04cfa6b7ecccecc1102b6
jakibaki <jakibaki@live.com>
2019-01-19 10:52:20 +0100

    Added simple rebind config system

--- hid_mitm/source/hid_mitm_iappletresource.cpp
+++ hid_mitm/source/hid_mitm_iappletresource.cpp
@@ -1,41 +1,57 @@
 
 #include <mutex>
+#include <map>
 #include <switch.h>
 #include "hid_mitm_iappletresource.hpp"
 
 static SharedMemory fake_shmem = {0};
-static HidSharedMemory* fake_shmem_mem;
-static HidSharedMemory* real_shmem_mem;
+static HidSharedMemory *fake_shmem_mem;
+static HidSharedMemory *real_shmem_mem;
 static HidSharedMemory tmp_shmem_mem;
 
 static Thread shmem_patch_thread;
 
-void copy_thread(void* _)
-{
-    while(true)
-    {
-        svcSleepThread(5000000);
-        tmp_shmem_mem = *real_shmem_mem;
-        for(int i = 0; i < 10; i++) {
-            for(int l = 0; l < 7; l++) {
-                HidControllerLayout *currentLayout = &tmp_shmem_mem.controllers[i].layouts[l];
-                for(int j = 0; j < 17; j++) {
-                    HidControllerInputEntry* curEnt = &currentLayout->entries[j];
-                    int a_held = curEnt->buttons & KEY_A;
-                    int b_held = curEnt->buttons & KEY_B;
-                    if(a_held) {
-                        curEnt->buttons |= KEY_B;
-                    } else {
-                        curEnt->buttons &= ~KEY_B;
-                    }
+std::map<u64, u64> rebind_config;
 
-                    if(b_held) {
-                        curEnt->buttons |= KEY_A;
-                    } else {
-                        curEnt->buttons &= ~KEY_A;
+void loadConfig(std::map<u64, u64> &cfg)
+{
+    cfg.clear();
+    cfg[KEY_A] = KEY_B;
+    cfg[KEY_B] = KEY_A;
+}
+
+void copy_thread(void *_)
+{
+    u64 curTime = svcGetSystemTick();
+    u64 oldTime;
+    while (true)
+    {
+        oldTime = curTime;
+        curTime = svcGetSystemTick();
+
+        svcSleepThread(std::max(1000L, 10000000 - (s64) ((curTime - oldTime) * 0.0192)));
+
+        tmp_shmem_mem = *real_shmem_mem;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int l = 0; l < 7; l++)
+            {
+                HidControllerLayout *currentTmpLayout = &tmp_shmem_mem.controllers[i].layouts[l];
+                HidControllerLayout *currentRealLayout = &real_shmem_mem->controllers[i].layouts[l];
+                for (int j = 0; j < 17; j++)
+                {
+                    HidControllerInputEntry *curTmpEnt = &currentTmpLayout->entries[j];
+                    HidControllerInputEntry *curRealEnt = &currentRealLayout->entries[j];
+
+                    for(auto const& [orig, fake]: rebind_config)
+                    {
+                        bool key_held = !!(curRealEnt->buttons & fake);
+                        if(key_held)
+                            curTmpEnt->buttons |= orig;
+                        else
+                            curTmpEnt->buttons &= ~orig;
                     }
                 }
-
             }
         }
         *fake_shmem_mem = tmp_shmem_mem;
@@ -44,27 +60,26 @@ void copy_thread(void* _)
 
 Result IAppletResourceMitmService::GetSharedMemoryHandle(Out<CopiedHandle> shmem_hand)
 {
-    if(fake_shmem.handle == NULL) {
+    if (fake_shmem.handle == NULL)
+    {
         shmemCreate(&fake_shmem, sizeof(HidSharedMemory), Perm_Rw, Perm_R);
         shmemMap(&fake_shmem);
-        fake_shmem_mem = (HidSharedMemory*) shmemGetAddr(&fake_shmem);
-        real_shmem_mem = (HidSharedMemory*) hidGetSharedmemAddr();
+        fake_shmem_mem = (HidSharedMemory *)shmemGetAddr(&fake_shmem);
+        real_shmem_mem = (HidSharedMemory *)hidGetSharedmemAddr();
+
+        // Copy over for the first time to make sure that there never is a "bad" sharedmem to be seen
         *fake_shmem_mem = *real_shmem_mem;
-        threadCreate(&shmem_patch_thread, copy_thread, NULL, 0x1000, 0x3F, 3);
+        loadConfig(rebind_config);
+        threadCreate(&shmem_patch_thread, copy_thread, NULL, 0x1000, 0x2C, 3);
         threadStart(&shmem_patch_thread);
     }
-    
-
-
-    //SharedMemory* shmem = hidGetSharedmem();
 
     shmem_hand.SetValue(fake_shmem.handle);
-
-
 
     return 0;
 }
 
-void IAppletResourceMitmService::PostProcess(IMitmServiceObject *obj, IpcResponseContext *ctx) {
-    /* No commands need postprocessing. */    
+void IAppletResourceMitmService::PostProcess(IMitmServiceObject *obj, IpcResponseContext *ctx)
+{
+    /* No commands need postprocessing. */
 }
